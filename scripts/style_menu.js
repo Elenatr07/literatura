d67ff6e59605172758f733d9f2f36e1f8970d34b
Elenatr07 <e.turkina2010@yandex.ru>
2023-06-04 22:32:05 +1000

    1

---
+++ scripts/style_menu.js
@@ -0,0 +1,13 @@
+//var input = document.querySelector('#side-menu')
+//input.addEventListener('click', f)
+//function f() {
+//    document.body.classList.toggle('nav_shadow')
+//}
+
+var elem = document.querySelector('#nav_shadow');
+var input = document.querySelector('#side-menu')
+input.addEventListener('click', function() {
+    elem.classList.toggle("nav_shadow");
+})
+
+
---
+++ sender.php
@@ -0,0 +1,28 @@
+<?php
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+
+	$to = "energotr07@yandex.ru"; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$from = $email;
+	$subject = "Заявка c сайта";
+    
+    
+
+	$msg="
+    Имя: $name /n
+    Телефон: $phone /n
+    Почта: $email /n
+    Текст: $text"; 	
+	mail($to, $subject, $msg, "From: $from ");
+
+    
+ 
+    
+       
+?>
+
+<p>Привет, форма отправлена</p>
