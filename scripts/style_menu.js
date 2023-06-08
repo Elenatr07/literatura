06ce55c7f2a9380bc895be252bf9f3506aeea6c1
Elenatr07 <e.turkina2010@yandex.ru>
2023-06-08 22:11:32 +1000

    16

--- scripts/style_menu.js
+++ scripts/style_menu.js
@@ -11,3 +11,28 @@ input.addEventListener('click', function() {
 })
 
 
+function imagefun() {
+    var Image_Id = document.getElementById('news1');
+    if (Image_Id.src.match("img/Group_40065.svg")) {
+        Image_Id.src = "img/Group_40080.svg";
+    }
+    else {
+        Image_Id.src = "img/Group_40065.svg";
+    }
+
+    var Image_Id1 = document.getElementById('news2');
+    if(Image_Id1.src.match("img/Group_40063.svg")) {
+        Image_Id1.src = "img/Group_40078.svg";
+    }
+    else {
+        Image_Id1.src = "img/Group_40063.svg"
+    }
+
+    var Image_Id2 = document.getElementById('lightiing');
+    if(Image_Id2.src.match("img/lighting.svg")) {
+        Image_Id2.src = "img/moon.svg";
+    }
+    else {
+        Image_Id2.src = "img/lighting.svg"
+    }
+ }
