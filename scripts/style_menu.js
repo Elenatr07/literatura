be73dd79119048d7606045f2bf6ee4b23cba3c22
Elenatr07 <e.turkina2010@yandex.ru>
2023-06-09 00:12:51 +1000

    18

--- scripts/style_menu.js
+++ scripts/style_menu.js
@@ -12,6 +12,21 @@ input.addEventListener('click', function() {
 
 
 function imagefun() {
+    var logo = document.getElementById('logo');
+    if (logo.src.match("img/logo_light.svg")) {
+        logo.src = "img/logo_dark.svg";
+    }
+    else {
+        logo.src = "img/logo_light.svg"
+    }
+
+    var logo = document.getElementById('footer_logo');
+    if (logo.src.match("img/logo_dark.svg")) {
+        logo.src = "img/logo_light.svg";
+    }
+    else {
+        logo.src = "img/logo_dark.svg"
+    }
     var Image_Id = document.getElementById('news1');
     if (Image_Id.src.match("img/Group_40065.svg")) {
         Image_Id.src = "img/Group_40080.svg";
@@ -36,21 +51,7 @@ function imagefun() {
         Image_Id2.src = "img/lighting.svg"
     }
 
-    var logo = document.getElementById('logo');
-    if (logo.src.match("img/logo_dark.svg")) {
-        logo.src = "img/logo_light.svg";
-    }
-    else {
-        logo.src = "img/logo_dark.svg"
-    }
-
-    var logo = document.getElementById('footer_logo');
-    if (logo.src.match("img/logo_dark.svg")) {
-        logo.src = "img/logo_light.svg";
-    }
-    else {
-        logo.src = "img/logo_dark.svg"
-    }
+  
     
  }
 
